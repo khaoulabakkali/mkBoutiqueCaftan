7a08e946f9982897bd8077a6dde27af543918222
khaoula bakkali <khaoula.bakkali@foliatech.ma>
2025-12-01 00:59:50 +0100

    fix translate

--- src/app/parametres/parametres.page.ts
+++ src/app/parametres/parametres.page.ts
@@ -8,13 +8,23 @@ import {
   IonMenuButton,
   IonList,
   IonItem,
+  IonItemDivider,
   IonLabel,
-  IonIcon
+  IonIcon,
+  ToastController
 } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslationService } from '../services/translation.service';
 import { addIcons } from 'ionicons';
-import { shieldCheckmark, resize, grid, settings } from 'ionicons/icons';
+import { shieldCheckmark, resize, grid, settings, globe, language, checkmark } from 'ionicons/icons';
+
+interface Language {
+  code: string;
+  name: string;
+  nativeName: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-parametres',
@@ -29,6 +39,7 @@ import { shieldCheckmark, resize, grid, settings } from 'ionicons/icons';
     IonMenuButton,
     IonList,
     IonItem,
+    IonItemDivider,
     IonLabel,
     IonIcon,
     CommonModule,
@@ -36,6 +47,14 @@ import { shieldCheckmark, resize, grid, settings } from 'ionicons/icons';
   ],
 })
 export class ParametresPage implements OnInit {
+  currentLanguage: string = 'fr';
+  currentLanguageName: string = 'Français';
+  availableLanguages: Language[] = [
+    { code: 'fr', name: 'French', nativeName: 'Français', icon: 'language' },
+    { code: 'en', name: 'English', nativeName: 'English', icon: 'language' },
+    { code: 'ar', name: 'Arabic', nativeName: 'العربية', icon: 'language' }
+  ];
+
   menuItems = [
     {
       titleKey: 'parameters.roles',
@@ -60,11 +79,49 @@ export class ParametresPage implements OnInit {
     }
   ];
 
-  constructor(private router: Router) {
-    addIcons({ shieldCheckmark, resize, grid, settings });
+  constructor(
+    private router: Router,
+    private translationService: TranslationService,
+    private translate: TranslateService,
+    private toastController: ToastController
+  ) {
+    addIcons({ shieldCheckmark, resize, grid, settings, globe, language, checkmark });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.currentLanguage = this.translationService.getCurrentLanguage();
+    this.updateCurrentLanguageName();
+    
+    // Écouter les changements de langue
+    this.translationService.currentLanguage$.subscribe(lang => {
+      this.currentLanguage = lang;
+      this.updateCurrentLanguageName();
+    });
+  }
+
+  updateCurrentLanguageName() {
+    const lang = this.availableLanguages.find(l => l.code === this.currentLanguage);
+    if (lang) {
+      this.currentLanguageName = lang.nativeName;
+    }
+  }
+
+  async changeLanguage(languageCode: string) {
+    if (languageCode !== this.currentLanguage) {
+      this.translationService.setLanguage(languageCode);
+      this.currentLanguage = languageCode;
+      this.updateCurrentLanguageName();
+      
+      // Afficher un message de confirmation
+      const toast = await this.toastController.create({
+        message: this.translate.instant('parameters.language.changed'),
+        duration: 2000,
+        color: 'success',
+        position: 'top'
+      });
+      await toast.present();
+    }
+  }
 
   navigateTo(route: string) {
     this.router.navigate([route]);
